0185372965f44fa5589dfefc918e37362d20bb6f
Buddhi Prasad <bp980930@gmail.com>
2024-08-11 07:24:29 +0530

    build error fixed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import logo from "@/../public/logo.png";
+import logo from "../../public/vercel.svg";
 
 const Logo = () => {
   return (
